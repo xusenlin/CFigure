1d2f1a6a614af26dc039867dda784d9bd4b7af0b
xusenlin <737657495@qq.com>
2016-11-07 09:55:46 +0800

    2016.11.07
    修改了更友好的api。添加了详细说明。

--- CFigure.js
+++ CFigure.js
@@ -1,1 +1,89 @@
-/*CFigure BY JORDAN 2016.07.29.Author Home:xusenlin.com*/var CFigure={option:{"id":"","img":{},"with":1170,"height":510,"time":2000},config:function(op){for(var k in this.option){if(op[k])this.option[k]=op[k];}},init:function(){var container=document.getElementById(this.option.id);var html='';var imgData=this.option.img;var imgWidth=this.option.with;var imgLenght=Object.keys(imgData).length;var userTime=this.option.time;html+='<div class="cfigure" style="width: '+imgWidth+'px;height: '+this.option.height+'px">'+'<ul id='+this.option.id+'-ul'+' class="cfigure-photo" style="width: '+imgWidth*imgLenght+'px;height: '+this.option.height+'px">';for(var i in imgData){html+='<li  style="width: '+imgWidth+'px;height: '+this.option.height+'px" class="cfigure-photo-li"><a target="_blank" href="';html+=imgData[i][1];html+='" class="cfigure-photo-a"';html+='title="'+i+'"';html+='><img src="';html+=imgData[i][0];html+='" alt="';html+=i;html+='" class="cfigure-img"></a></li>';}html+='</ul>';html+='<ul class="cfigure-point" id="'+this.option.id+'-point'+'">';for(var i=0;i<imgLenght;i++){html+='<li class="cfigure-point-li"></li>';};html+='</ul></div>';container.innerHTML=html;var imgBox=document.getElementById(this.option.id+'-ul');var pointGroup=document.getElementById(this.option.id+'-point');var point=pointGroup.querySelectorAll('.cfigure-point-li');point[0].className='cfigure-point-li active';var varImg=1;var interval=setInterval(function(){if(varImg==imgLenght){varImg=0;};for(var i=0;i<point.length;i++){point[i].className='cfigure-point-li';};point[varImg].className='cfigure-point-li active';imgBox.style.left=-(imgWidth*varImg)+'px';varImg++;},userTime);pointGroup.onmousemove=function(){clearInterval(interval);};pointGroup.onmouseout=function(){interval=setInterval(function(){if(varImg==imgLenght){varImg=0;};for(var i=0;i<point.length;i++){point[i].className='cfigure-point-li';};point[varImg].className='cfigure-point-li active';imgBox.style.left=-(imgWidth*varImg)+'px';varImg++;},userTime);};for(var i=0;i<point.length;i++){(function(){var p=i;point[i].onclick=function(){varImg=p;for(var i=0;i<point.length;i++){point[i].className='cfigure-point-li';};point[varImg].className='cfigure-point-li active';imgBox.style.left=-(imgWidth*varImg)+'px';varImg++;};})();};}};
+/*CFigure BY JORDAN 2016.07.29.Author Home:xusenlin.com*/
+var CFigure = {
+    option: {"id": "", "img": {}, "with": 1170, "height": 510, "time": 2000}, config: function (op) {
+        for (var k in this.option) {
+            if (op[k])this.option[k] = op[k];
+        }
+        this.init();
+    },
+    init: function () {
+        var container = document.getElementById(this.option.id);
+        var html = '';
+        var imgData = this.option.img;
+        var imgWidth = this.option.with;
+        var imgLenght = Object.keys(imgData).length;
+        var userTime = this.option.time;
+        html += '<div class="cfigure" style="width: ' + imgWidth + 'px;height: ' + this.option.height + 'px">' + '<ul id=' + this.option.id + '-ul' + ' class="cfigure-photo" style="width: ' + imgWidth * imgLenght + 'px;height: ' + this.option.height + 'px">';
+        for (var i in imgData) {
+            html += '<li  style="width: ' + imgWidth + 'px;height: ' + this.option.height + 'px" class="cfigure-photo-li"><a target="_blank" href="';
+            html += imgData[i][1];
+            html += '" class="cfigure-photo-a"';
+            html += 'title="' + i + '"';
+            html += '><img src="';
+            html += imgData[i][0];
+            html += '" alt="';
+            html += i;
+            html += '" class="cfigure-img"></a></li>';
+        }
+        html += '</ul>';
+        html += '<ul class="cfigure-point" id="' + this.option.id + '-point' + '">';
+        for (var i = 0; i < imgLenght; i++) {
+            html += '<li class="cfigure-point-li"></li>';
+        }
+        ;
+        html += '</ul></div>';
+        container.innerHTML = html;
+        var imgBox = document.getElementById(this.option.id + '-ul');
+        var pointGroup = document.getElementById(this.option.id + '-point');
+        var point = pointGroup.querySelectorAll('.cfigure-point-li');
+        point[0].className = 'cfigure-point-li active';
+        var varImg = 1;
+        var interval = setInterval(function () {
+            if (varImg == imgLenght) {
+                varImg = 0;
+            }
+            ;
+            for (var i = 0; i < point.length; i++) {
+                point[i].className = 'cfigure-point-li';
+            }
+            ;
+            point[varImg].className = 'cfigure-point-li active';
+            imgBox.style.left = -(imgWidth * varImg) + 'px';
+            varImg++;
+        }, userTime);
+        pointGroup.onmousemove = function () {
+            clearInterval(interval);
+        };
+        pointGroup.onmouseout = function () {
+            interval = setInterval(function () {
+                if (varImg == imgLenght) {
+                    varImg = 0;
+                }
+                ;
+                for (var i = 0; i < point.length; i++) {
+                    point[i].className = 'cfigure-point-li';
+                }
+                ;
+                point[varImg].className = 'cfigure-point-li active';
+                imgBox.style.left = -(imgWidth * varImg) + 'px';
+                varImg++;
+            }, userTime);
+        };
+        for (var i = 0; i < point.length; i++) {
+            (function () {
+                var p = i;
+                point[i].onclick = function () {
+                    varImg = p;
+                    for (var i = 0; i < point.length; i++) {
+                        point[i].className = 'cfigure-point-li';
+                    }
+                    ;
+                    point[varImg].className = 'cfigure-point-li active';
+                    imgBox.style.left = -(imgWidth * varImg) + 'px';
+                    varImg++;
+                };
+            })();
+        }
+        ;
+    }
+};
--- README.md
+++ README.md
@@ -1,7 +1,117 @@
 
 CFigure BY JORDAN 2016.07.29.
+-----------------------------
+
+> Author Home:xusenlin.com.---
 
 CFigure是一个方便，简单，定制性好，轻量级，代码体积5k，压缩后3k。只需3步即可在页面上添加不止一个漂亮的可互交的轮播图插件。不依赖于任何js库。
 
-使用方法请查看index.html
+需要的html:
 
+    <div id="cf"></div>
+    <div id="hh"></div>
+
+这里可以添加一到多个容器，从而产生多幅轮播图。
+
+
+----------
+css:
+
+    <style>
+		*{
+			padding: 0;
+			margin: 0;
+		}
+
+		.cfigure{
+			position: relative;
+			overflow: hidden;
+			margin: 0;
+			padding: 0;
+		}
+		.cfigure-photo{
+			margin: 0;
+			padding: 0;
+			position: absolute;
+			left: 0;
+			list-style-type: none;
+			transition: all 0.4s;
+		}
+		.cfigure-photo-li{
+			margin: 0;
+			padding: 0;
+			float: left;
+		}
+		.cfigure-photo-a{
+			margin: 0;
+			padding: 0;
+			font-size: 0;
+		}
+		.cfigure-img{
+			margin: 0;
+			padding: 0;
+			width: 100%;
+			height: auto;
+		}
+		.cfigure-point{
+			position: absolute;
+			transition: all 0.4s;
+			list-style-type: none;
+			left: 36px;
+			bottom: 26px;
+			height: 11px;
+
+		}
+		.cfigure-point-li{
+			cursor: pointer;
+			float: left;
+			width: 11px;
+			height: 11px;
+			margin-right: 10px;
+			border-radius: 50%;
+			background: #9f9fa0;
+			border: 2px solid #9f9fa0;
+		}
+		.cfigure-point .active,.cfigure-point-li:hover {
+			background: #dc1716;
+			border: 2px solid #bcbbbb;
+		}
+	</style>
+
+这一步是必须的。使你的轮播图更美观。
+
+
+----------
+
+js:
+
+    <script>
+    	CFigure.config({
+    		"id":"cf",
+    		"with":1170,
+    		"height":510,
+    		"img":{
+    			"1图片描述":["img/1.jpg","http://localhost/wordpress/"],
+    			"2图片描述":["img/2.jpg","http://localhost/wordpress/"],
+    			"3图片描述":["img/3.jpg","http://localhost/wordpress/"],
+    			"4图片描述":["img/4.jpg","http://localhost/wordpress/"]
+    		}
+    	});
+    
+    	CFigure.config({
+    		"id":"hh",
+    		"with":700,
+    		"height":260,
+    		"time":1000,
+    		"img":{
+    			"1图片描述":["img/3.jpg","http://localhost/wordpress/"],
+    			"2图片描述":["img/2.jpg","http://localhost/wordpress/"],
+    			"3图片描述":["img/1.jpg","http://localhost/wordpress/"],
+    			"4图片描述":["img/4.jpg","http://localhost/wordpress/"]
+    		}
+    	});
+    
+    </script>
+
+这里是轮播图配置，相信你能看得懂，祝使用愉快！
+如果喜欢，别忘了给一个Star-_-
